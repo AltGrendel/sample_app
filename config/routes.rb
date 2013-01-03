d525868d0b2708d7f5c778e2bde6efb3b3adf9b0
AltGrendel <altgrendel@exit0.us>
2013-01-02 21:26:30 -0500

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
 #  get "users/new"
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy ]
 
   root :to => 'static_pages#home'
   match '/signup', to: 'users#new'
@@ -9,5 +10,7 @@ SampleApp::Application.routes.draw do
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
 end
